ea32c134592d92b0b4c4e0917e79672a5ad76a40
emylz <eminyalaz@gmail.com>
2018-11-27 23:08:35 +0100

    test

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,5 +1,47 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
 public class Controller {
+
+    @FXML
+    Label listLab;
+
+    @FXML
+    Label sellinLab;
+
+    @FXML
+    Label qualityLab;
+
+    @FXML
+    Label nameLab;
+
+    @FXML
+    Label typeLab;
+
+    @FXML
+    ListView<String> listItem;
+
+    @FXML
+    TextField sellinField;
+
+    @FXML
+    TextField identityLab;
+
+    @FXML
+    TextField nameField;
+
+    @FXML
+    TextField typeField;
+
+    @FXML
+    Button updateButton;
+
+
 
 }
