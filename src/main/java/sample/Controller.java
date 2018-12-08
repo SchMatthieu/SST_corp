2e155cff5b4ffa5f5f0e76f0b80faa9946116980
chickennem <xavsy@hotmail.com>
2018-12-08 17:51:33 +0100

    Il n'y a plus d'erreur

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,6 +1,8 @@
 package sample;
 
 import edu.insightr.gildedrose.Item;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -41,18 +43,16 @@ public class Controller {
     @FXML
     TextField typeField;
 
+    @SuppressWarnings("deprecation")
     @FXML
-    final Button updateButton = new Button("updateButton");
+    Button updateButton = new Button("Update");
 
     List<Item> listOfItem = new LinkedList<>();
 
-    private Item fetchItem(String name)
-    {
+    private Item fetchItem(String name) {
         Item item = null;
-        for(int i = 0; i < listOfItem.size();i++)
-        {
-            if(listOfItem.get(i).getName() == name)
-            {
+        for (int i = 0; i < listOfItem.size(); i++) {
+            if (listOfItem.get(i).getName() == name) {
                 item = listOfItem.get(i);
                 break;
             }
@@ -60,72 +60,74 @@ public class Controller {
         return item;
     }
 
-    private void displaDetail(String name)
-    {
-        try
-        {
+    private void displaDetail(String name) {
+        try {
             Item item = fetchItem(name);
             quality.setText(item.getName());
             nameField.setText(item.getName());
             sellinField.setText(String.valueOf(item.getSellIn()));
             typeField.setText(String.valueOf(item.getClass()));
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+    public void update() throws Exception{
 
-          public void updateInventory()
-        {
-            Item[] items = listItem;
+        @SuppressWarnings("deprecation")
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
 
-            for(int i = 0; i < items.length; i++)
-            {
-                String nom = items[i].getName();
-                switch (nom)
-                {
-                    case "+5 Dexterity Vest":
-                        System.out.println(items[i].toString());
+            public void handle(ActionEvent e) {
+                Item[] items = listItem;
 
-                        System.out.println(items[i].toString());
-                        break;
+                for (int i = 0; i < items.length; i++) {
+                    String nom = items[i].getName();
+                    switch (nom) {
+                        case "+5 Dexterity Vest":
+                            System.out.println(items[i].toString());
 
-                    case "Aged Brie":
-                        System.out.println(items[i].toString());
+                            System.out.println(items[i].toString());
+                            break;
 
-                        System.out.println(items[i].toString());
-                        break;
+                        case "Aged Brie":
+                            System.out.println(items[i].toString());
 
-                    case "Elixir of the Mongoose":
-                        System.out.println(items[i].toString());
+                            System.out.println(items[i].toString());
+                            break;
 
-                        System.out.println(items[i].toString());
-                        break;
+                        case "Elixir of the Mongoose":
+                            System.out.println(items[i].toString());
 
-                    case "Sulfuras, Hand of Ragnaros":
-                        System.out.println(items[i].toString());
+                            System.out.println(items[i].toString());
+                            break;
 
-                        System.out.println(items[i].toString());
-                        break;
+                        case "Sulfuras, Hand of Ragnaros":
+                            System.out.println(items[i].toString());
 
-                    case "Backstage passes to a TAFKAL80ETC concert":
-                        System.out.println(items[i].toString());
+                            System.out.println(items[i].toString());
+                            break;
 
-                        System.out.println(items[i].toString());
-                        break;
+                        case "Backstage passes to a TAFKAL80ETC concert":
+                            System.out.println(items[i].toString());
 
-                    case "Conjured Mana Cake":
-                        System.out.println(items[i].toString());
+                            System.out.println(items[i].toString());
+                            break;
 
-                        items[i].toString();
-                        break;
+                        case "Conjured Mana Cake":
+                            System.out.println(items[i].toString());
 
-                    default:
-                        break;
+                            items[i].toString();
+                            break;
+
+                        default:
+                            break;
+                    }
                 }
             }
-        }
+        };
+        updateButton.setOnAction(event);
+
     }
+}
+
 
