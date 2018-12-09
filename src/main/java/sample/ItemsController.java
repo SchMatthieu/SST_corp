86985628eff3fe4e2c536b49064b4879d7cc0867
matts <matthieu.schoeser@devinci.fr>
2018-12-09 21:28:25 +0100

    Liste affichée

--- src/main/java/sample/ItemsController.java
+++ src/main/java/sample/ItemsController.java
@@ -5,6 +5,7 @@ import edu.insightr.gildedrose.Item;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -13,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class ItemsController{
+public class ItemsController implements Initializable {
 
     @FXML
     TextField textfield_name;
