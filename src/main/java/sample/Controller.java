8c4df1c9cbff1f4badef32ebab1b1895140d9585
emylz <eminyalaz@gmail.com>
2018-12-10 15:57:08 +0100

    Test

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -4,10 +4,16 @@ import edu.insightr.gildedrose.Inventory;
 import edu.insightr.gildedrose.Item;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
+import javafx.scene.chart.*;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Group;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -26,6 +32,8 @@ public class Controller implements Initializable {
     TextField typeField;
     @FXML
     ListView<String> list_items;
+    @FXML
+    PieChart pie;
 
     public Inventory inventory;
 
@@ -33,6 +41,7 @@ public class Controller implements Initializable {
         fetchItems();
         list_items.getSelectionModel().selectedItemProperty().addListener(e->
                 displayItemsDetails(list_items.getSelectionModel().getSelectedItem()));
+        pieChart();
     }
     private Item fetchItemByName(String name)
     {
@@ -83,6 +92,27 @@ public class Controller implements Initializable {
     public void updateQuality(){
         this.inventory.updateQuality();
         displayItemsDetails(inventory);
+
+    }
+
+
+    public void pieChart()
+    {
+        this.inventory.proportion();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("Aged_Brie", this.inventory.getProportion()[0]),
+                new PieChart.Data("Backstage_passes_to_a_TAFKAL80ETC_concert", this.inventory.getProportion()[1]),
+                new PieChart.Data("Conjured_Mana_Cake", this.inventory.getProportion()[2]),
+                new PieChart.Data("Dexterity_Vest", this.inventory.getProportion()[3]),
+                new PieChart.Data("Elixir_of_the_Mongoose", this.inventory.getProportion()[4]),
+                new PieChart.Data("Sulfuras_Hand_of_Ragnaros", this.inventory.getProportion()[5]));
+
+       pie.setData(pieChartData);
+       pie.setLegendSide(Side.BOTTOM);
+       pie.setLabelLineLength(100);
+       pie.setLabelsVisible(true);
+       pie.setTitle("Proportion of each item");
+
 
     }
 
