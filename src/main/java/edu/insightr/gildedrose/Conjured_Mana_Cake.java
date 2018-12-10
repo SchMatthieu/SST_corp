82f9a057d7820c9f7c573a7f365089ab8b5d7253
emylz <43544835+emylz@users.noreply.github.com>
2018-12-10 09:43:57 +0100

    Update Conjured_Mana_Cake.java

--- src/main/java/edu/insightr/gildedrose/Conjured_Mana_Cake.java
+++ src/main/java/edu/insightr/gildedrose/Conjured_Mana_Cake.java
@@ -23,13 +23,16 @@ public class Conjured_Mana_Cake extends Item{
 
     public void updateQuality(){
         super.updateQuality();
-        if(this.quality >= 2){
+         if(this.quality == 1) this.quality = 0;
+        
+        else if(this.quality >= 2){
+            
             this.quality  = this.quality  - 2;
+            
             if (this.sellIn == 0 &&  this.quality >= 2){
                 this.quality  = this.quality  - 2;
             }
         }
-        if(this.quality == 1) this.quality = 0;
         
     }
 }
